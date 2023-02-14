3c6136611cc7ef6df138e4ddd6e1e41ef8d5af2d
BTNguyen9280 <briannguyen9280@yahoo.com>
2023-02-13 20:48:41 -0800

    Add basic math operator functions

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const add = function(a,b) {
+    return a+b
+  };
+  
+  const subtract = function(a,b) {
+    return a-b	
+  };
+  
+  const sum = function(a) {
+    return a.reduce((total,sum1) => total+sum1,0
+  )};
+  
+  const multiply = function(a,b) {
+    return a*b
+  };
+  
+ const divide = function(a,b) {
+    return a/b
+ }
