e7fc00753bfcd9a376cbe36e6b7c409e18e138ca
BTNguyen9280 <briannguyen9280@yahoo.com>
2023-02-16 21:28:33 -0800

    fix UI, initialize JS

--- javascript.js
+++ javascript.js
@@ -1,3 +1,27 @@
+const one = document.querySelector('#one') 
+const two = document.querySelector('#two')
+const three = document.querySelector('#three')
+const four = document.querySelector('#four')
+const five = document.querySelector('#five')
+const six = document.querySelector('#six')
+const seven = document.querySelector('#seven')
+const eight = document.querySelector('#eight')
+const nine = document.querySelector('#nine')
+const zero = document.querySelector('#zero')
+const decimal = document.querySelector('#decimal')
+const addOperator = document.querySelector('#add')
+const subtractOperator = document.querySelector('#subtract')
+const multiplyOperator = document.querySelector('#multiply')
+const divideOperator = document.querySelector('#divide')
+const equalOperator = document.querySelector('#equals')
+const display1 = document.querySelector('.display1')
+const display2 = document.querySelector('.display2')
+const clear = document.querySelector('#clear')
+const CE = document.querySelector('#ce')
+
+
+
+
 const add = function(a,b) {
     return a+b
   };
@@ -18,7 +42,7 @@ const add = function(a,b) {
     return a/b
  }
 
- const operate = function (op,a,b) {
+ const operate = function (a,op,b) {
     if (op == '+') {
         return add(a,b)
     }
@@ -32,3 +56,19 @@ const add = function(a,b) {
         return divide(a,b)
     }
  }
+
+let num1 ; 
+let num2 ;
+let operator ;
+
+CE.addEventListener('click', function (e) {
+  display2.textContent = '';
+  num2 = ''
+})
+
+clear.addEventListener('click', function(e) {
+  display1.textContent = '';
+  display2.textContent='';
+  num1 = ''
+  num2 = ''
+})
